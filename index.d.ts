cadd9ab7eb18167094e14ddb8ca39524c69d4f08
颜海镜 <yanhaijing@yeah.net>
2019-03-20 13:15:01 +0800

    0.8.2

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,14 @@
 export as namespace jsminiIs;
 
 export function isInRange (x: number, min?: number, max?: number): boolean;
-export function isNumber (x: any, min?: number, max?: number): boolean;
-export function isInteger (x: any, min?: number, max?: number): boolean;
-export function isInt (x: any): boolean;
-export function isBoolean (x: any): boolean;
-export function isString (x: any): boolean;
+export function isNumber (x: any, min?: number, max?: number): x is number;
+export function isInteger (x: any, min?: number, max?: number): x is number;
+export function isInt (x: any): x is number;
+export function isBoolean (x: any): x is boolean;
+export function isString (x: any): x is string;
 export function isEmptyString (x: any): boolean;
-export function isNull (x: any): boolean;
-export function isUndefined (x: any): boolean;
-export function isObject (x: any): boolean;
-export function isFunction (x: any): boolean;
-export function isArray (x: any): boolean;
+export function isNull (x: any): x is null;
+export function isUndefined (x: any): x is undefined;
+export function isObject (x: any): x is object;
+export function isFunction (x: any): x is (...any) => any;
+export function isArray (x: any): x is any[];
